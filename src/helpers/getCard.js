98857ddd3f595b55b214be745496750ed4d47c4c
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-06-26 06:01:58 +0000

    fixed urls shorten

--- src/helpers/getCard.js
+++ src/helpers/getCard.js
@@ -1,212 +1,163 @@
 export const generatePokerCard = (card, image) => {
+  const url =
+    "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/";
+
   switch (card) {
     case "1clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/AC.svg";
+      image.src = `${url}AC.svg`;
       break;
     case "1diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/AD.svg";
+      image.src = `${url}AD.svg`;
       break;
     case "1hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/AH.svg";
+      image.src = `${url}AH.svg`;
       break;
     case "1spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/AS.svg";
+      image.src = `${url}AS.svg`;
       break;
     case "2clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/2C.svg";
+      image.src = `${url}2C.svg`;
       break;
     case "2diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/2D.svg";
+      image.src = `${url}2D.svg`;
       break;
     case "2hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/2H.svg";
+      image.src = `${url}2H.svg`;
       break;
     case "2spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/2S.svg";
+      image.src = `${url}2S.svg`;
       break;
     case "3clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/3C.svg";
+      image.src = `${url}3C.svg`;
       break;
     case "3diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/3D.svg";
+      image.src = `${url}3D.svg`;
       break;
     case "3hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/3H.svg";
+      image.src = `${url}3H.svg`;
       break;
     case "3spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/3S.svg";
+      image.src = `${url}3S.svg`;
       break;
     case "4clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/4C.svg";
+      image.src = `${url}4C.svg`;
       break;
     case "4diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/4D.svg";
+      image.src = `${url}4D.svg`;
       break;
     case "4hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/4H.svg";
+      image.src = `${url}4H.svg`;
       break;
     case "4spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/4S.svg";
+      image.src = `${url}4S.svg`;
       break;
     case "5clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/5C.svg";
+      image.src = `${url}5C.svg`;
       break;
     case "5diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/5D.svg";
+      image.src = `${url}5D.svg`;
       break;
     case "5hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/5H.svg";
+      image.src = `${url}5H.svg`;
       break;
     case "5spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/5S.svg";
+      image.src = `${url}5S.svg`;
       break;
     case "6clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/6C.svg";
+      image.src = `${url}6C.svg`;
       break;
     case "6diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/6D.svg";
+      image.src = `${url}6D.svg`;
       break;
     case "6hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/6H.svg";
+      image.src = `${url}6H.svg`;
       break;
     case "6spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/6S.svg";
+      image.src = `${url}6S.svg`;
       break;
     case "7clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/7C.svg";
+      image.src = `${url}7C.svg`;
       break;
     case "7diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/7D.svg";
+      image.src = `${url}7D.svg`;
       break;
     case "7hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/7H.svg";
+      image.src = `${url}7H.svg`;
       break;
     case "7spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/7S.svg";
+      image.src = `${url}7S.svg`;
       break;
     case "8clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/8C.svg";
+      image.src = `${url}8C.svg`;
       break;
     case "8diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/8D.svg";
+      image.src = `${url}8D.svg`;
       break;
     case "8hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/8H.svg";
+      image.src = `${url}8H.svg`;
       break;
     case "8spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/8S.svg";
+      image.src = `${url}8S.svg`;
       break;
     case "9clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/9C.svg";
+      image.src = `${url}9C.svg`;
       break;
     case "9diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/9D.svg";
+      image.src = `${url}9D.svg`;
       break;
     case "9hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/9H.svg";
+      image.src = `${url}9H.svg`;
       break;
     case "9spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/9S.svg";
+      image.src = `${url}9S.svg`;
       break;
     case "10clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/TC.svg";
+      image.src = `${url}TC.svg`;
       break;
     case "10diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/TD.svg";
+      image.src = `${url}TD.svg`;
       break;
     case "10hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/TH.svg";
+      image.src = `${url}TH.svg`;
       break;
     case "10spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/TS.svg";
+      image.src = `${url}TS.svg`;
       break;
     case "11clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/JC.svg";
+      image.src = `${url}JC.svg`;
       break;
     case "11diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/JD.svg";
+      image.src = `${url}JD.svg`;
       break;
     case "11hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/JH.svg";
+      image.src = `${url}JH.svg`;
       break;
     case "11spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/JS.svg";
+      image.src = `${url}JS.svg`;
       break;
     case "12clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/QC.svg";
+      image.src = `${url}QC.svg`;
       break;
     case "12diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/QD.svg";
+      image.src = `${url}QD.svg`;
       break;
     case "12hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/QH.svg";
+      image.src = `${url}QH.svg`;
       break;
     case "12spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/QS.svg";
+      image.src = `${url}QS.svg`;
       break;
     case "13clubs":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/KC.svg";
+      image.src = `${url}KC.svg`;
       break;
     case "13diamons":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/KD.svg";
+      image.src = `${url}KD.svg`;
       break;
     case "13hearts":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/KH.svg";
+      image.src = `${url}KH.svg`;
       break;
     case "13spades":
-      image.src =
-        "https://raw.githubusercontent.com/4GeeksAcademy/arroyostack-random-card-generator-poker/2581db635539c3834a4e1d7d57fdc7255c46a038/src/assets/poker-cards/KS.svg";
+      image.src = `${url}KS.svg`;
       break;
 
     default:
